77d3351d24b43db3672c0d56c88dadb0f56cd115
Uxío García Andrade <uxiog21@gmail.com>
2021-02-22 18:29:01 +0100

    add example

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use pagerank::Graph;
 
 fn main() {
     let mut g = Graph::new();
-    g.add_link(0,1,0.4);
-    g.add_link(0,1,0.3);
-    println!("{}",g.nodes[&0]);
+    g.add_link(1,2,1.0); 
+    g.add_link(1,3,2.0);
+    g.add_link(2,3,3.0);
+    g.add_link(2,4,4.0);
+    g.add_link(3,1,5.0);
+    g.page_rank(0.85, 0.000001, |node, rank| {println!("Node: {} - Rank: {}",node,rank); });
 }
